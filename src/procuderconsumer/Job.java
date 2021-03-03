01ec805cd5165d71dc16f885f61acb2ff558eeb3
gabrielaramburu <gabrielaramburu@gmail.com>
2021-03-02 21:22:45 -0300

    New multithread initializer example

---
+++ src/procuderconsumer/Job.java
@@ -0,0 +1,51 @@
+package procuderconsumer;
+
+public class Job {
+	protected static final int MAX_JOB_VALUE = 100;
+	protected static final int MAX_JOBS = 1000;
+	
+	private int id;
+	private int value;
+	private boolean finalJob;
+	
+	public Job(int id, int value) {
+		this.id = id;
+		this.value = value;
+		this.finalJob = false;
+	}
+	
+	public Job(boolean finalJob) {
+		this(0,0);
+		this.finalJob = finalJob;
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+
+	public boolean isFinalJob() {
+		return finalJob;
+	}
+
+	public void setFinalJob(boolean finalJob) {
+		this.finalJob = finalJob;
+	}
+
+	
+
+	
+}
---
+++ src/procuderconsumer/Main.java
@@ -0,0 +1,8 @@
+package procuderconsumer;
+
+public class Main {
+	public static void main(String[] args) throws InterruptedException {
+		TestProducerConsumer test = new TestProducerConsumer();
+		test.start();
+	}
+}
