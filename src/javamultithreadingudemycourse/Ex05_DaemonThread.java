a35c7daa74a7d073fd486544b35ab31890509a75
gabrielaramburu <gabrielaramburu@gmail.com>
2021-09-03 19:48:39 -0300

    Daemon Thread example with comments

--- src/javamultithreadingudemycourse/Ex05_DaemonThread.java
+++ src/javamultithreadingudemycourse/Ex05_DaemonThread.java
@@ -3,7 +3,12 @@ package javamultithreadingudemycourse;
 public class Ex05_DaemonThread {
     public static void main(String[] args) {
         DeamonThread t1 = new DeamonThread();
-        t1.setDaemon(true);
+        //when this line is commented the application keeps alive and show the message
+        //"End thread..." on console
+        //However when the threas is stting as Deamon, the last sentence on thred
+        //never is showen
+        
+        t1.setDaemon(true); 
         t1.start();
 
         System.out.println("last sentence of application");
@@ -13,11 +18,11 @@ public class Ex05_DaemonThread {
         public void run(){
             System.out.println("Waiting...");
             try {
-                this.sleep(10_000);
+                this.sleep(5_000);
             } catch (InterruptedException e) {
                 System.err.println("error.");
             }
-
+            System.out.println("End thread");
         }
     }
 }
