1cfd17e2787590cb6fa287b55a047f9b784338cd
gabrielaramburu <gabrielaramburu@gmail.com>
2021-09-18 10:52:50 -0300

    Train intersection dead lock example

---
+++ src/javamultithreadingudemycourse/Ex11_DeadLockExample.java
@@ -0,0 +1,78 @@
+package javamultithreadingudemycourse;
+
+public class Ex11_DeadLockExample {
+
+	public static void main(String[] args) {
+		Intersection inter = new Intersection();
+
+		Runnable train1 = () -> {
+			while(true) {
+				inter.takeRailA();
+				
+			}
+		};
+
+		Runnable train2 = () -> {
+			while(true) {
+				inter.takeRailB();
+				
+			}
+		};
+
+		Thread t1 = new Thread(train1);
+		Thread t2 = new Thread(train2);
+
+		t1.start();
+		t2.start();
+	}
+
+	
+}
+
+
+
+class Intersection {
+	Object railA;
+	Object railB;
+
+	public Intersection() {
+		this.railA = new Object();
+		this.railB = new Object();
+	}
+
+	void takeRailA() {
+		synchronized(railA) {
+			System.out.println(Thread.currentThread().getName() + " The rail A has been taken.");
+			synchronized(railB) {
+				System.out.println(Thread.currentThread().getName() + " The rail B has been taken.");
+				System.out.println("The trainA is passing on rail A.");
+				pause(500);
+				System.out.println(System.currentTimeMillis() + " The trainA has passed");
+				
+
+			}
+		}
+	}
+
+	void takeRailB() {
+		synchronized(railA) {
+			System.out.println(Thread.currentThread().getName() + " The rail B has been taken.");
+			synchronized(railB) {
+				System.out.println(Thread.currentThread().getName() + " The rail A has been taken.");
+				System.out.println("The trainB is passing on rail B.");
+				pause(500);
+				System.out.println(System.currentTimeMillis() + " The trainB has passed");
+				
+			}
+		}
+	}
+
+	 void pause(long pause) {
+		try {
+			Thread.sleep(pause);
+		} catch (InterruptedException e) {
+			System.err.println("error");
+		}
+
+	}
+}
