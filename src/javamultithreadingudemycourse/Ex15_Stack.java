64383c35cd3e23ff9ce342f26eb8e2728ce2f212
gabrielaramburu <gabrielaramburu@gmail.com>
2021-09-23 13:43:50 -0300

    stack with lockfree implementation

--- src/javamultithreadingudemycourse/Ex15_Stack.java
+++ src/javamultithreadingudemycourse/Ex15_Stack.java
@@ -1,10 +1,13 @@
 package javamultithreadingudemycourse;
 import java.util.*;
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.*;
+import java.util.concurrent.locks.*;
 
 public class Ex15_Stack {
     public static void main(String[] args) throws InterruptedException {
         StandarStack<Integer> stack = new StandarStack<Integer>();
+        //LockFreeStack<Integer> stack = new LockFreeStack<Integer>();
         ThreadLocalRandom random = ThreadLocalRandom.current();
 
         Thread t1 = new Thread(()->{
@@ -25,6 +28,49 @@ public class Ex15_Stack {
 
         Thread.sleep(10000);
         System.out.format("%,d number of operations \n", stack.getCounter());
+    }
+}
+
+class LockFreeStack<T> {
+    private AtomicReference<Node<T>> head = new AtomicReference<Node<T>>();
+    private AtomicInteger counter = new AtomicInteger(0);
+
+    public void push(T value) {
+        Node<T> newNode = new Node(value);
+        while(true) {
+            Node<T> currentHead = head.get();
+            newNode.next = currentHead;
+            if (head.compareAndSet(currentHead, newNode)) {
+                counter.incrementAndGet();        
+                break;
+            } else {
+                LockSupport.parkNanos(1);
+            }
+        }
+        //counter.incrementAndGet();
+
+    }
+
+    public T pop() {
+        Node<T> currentHeadNode = head.get();
+        Node<T> newHeadNode;
+
+        while (currentHeadNode != null)  {
+            newHeadNode = head.get().next;
+            if (head.compareAndSet(currentHeadNode, newHeadNode)) {
+               break;
+            } else {
+                LockSupport.parkNanos(1);
+                currentHeadNode = head.get();
+            }
+        }
+        counter.incrementAndGet();
+        return currentHeadNode != null ? currentHeadNode.value : null;
+    }
+
+    public Integer getCounter() {
+
+        return counter.get();
     }
 }
 
